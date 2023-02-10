4504fe5ab8dd841db709e6d990697f825dc0ca07
Tina Huynh <tinamchuynh@gmail.com>
2023-02-10 11:26:18 -0800

    use project list array

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const generateButton = document.querySelector("#generate-button");
-const minInput = document.querySelector("#min");
-const maxInput = document.querySelector("#max");
 const resultSpan = document.querySelector("#result");
 
+const projectIdeas = [
+  "Build a to-do list app",
+  "Create a weather app",
+  "Develop a recipe search engine",
+  "Build a job board platform",
+  "Create a news aggregator",
+  "Develop a workout tracker",
+  "Build a music player app",
+  "Create a budgeting tool",
+  "Develop a habit tracker",
+  "Build a real-time chat app"
+];
+
 generateButton.addEventListener("click", function() {
-  const min = Number(minInput.value);
-  const max = Number(maxInput.value);
-  const result = Math.floor(Math.random() * (max - min + 1) + min);
+  const result = projectIdeas[Math.floor(Math.random() * projectIdeas.length)];
   resultSpan.textContent = result;
 });
